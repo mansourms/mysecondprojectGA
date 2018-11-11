ce2d07751ecaf4b86001cfb5f67eb46abde03c49
mansourms <mms987987@gmail.com>
2018-11-11 15:16:02 +0300

    m

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
   resources :cars
   resources :companies
   resources :images
+  root "companies#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
