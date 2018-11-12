fb8c0b2fb9a0e4b50550481fb0d023f33f9dea30
mansourms <mms987987@gmail.com>
2018-11-12 16:51:08 +0300

    add app to heroku server

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -3,11 +3,33 @@ class ImagesController < ApplicationController
   end
 
   def show
-  end
-
-  def new
+    @image = Image.find_by(id: params[:id])
   end
 
   def edit
+    @image = Image.find_by(id: params[:id])
+  end
+
+  def update
+    image = Image.find_by(id: params[:id])
+    if params[:image]
+      req = Cloudinary::Uploader.upload(params[:image][:url])
+      image.update(url: req["url"])
+      redirect_to car_path(image.car.id)
+    else
+      redirect_to edit_image_path(image)
+    end
+  end
+
+  def destroy
+    image = Image.find_by(id: params[:id])
+    image.destroy
+    redirect_to cars_path
+  end
+
+  private
+
+  def image_params
+    params.require(:image).permit(:url)
   end
 end
