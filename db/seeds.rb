64e2f5a6539b37a172e3813478fa30372e13a4c0
mansourms <mms987987@gmail.com>
2018-11-12 17:03:39 +0300

    ssss

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,16 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 Company.destroy_all
 
-toyota = Company.create(name: "Toyota", country: "Japan")
-nissan = Company.create(name: "Nissan", country: "Japan")
+# toyota = Company.create(name: "Toyota", country: "Japan")
+# nissan = Company.create(name: "Nissan", country: "Japan")
 
 Car.destroy_all
 
-camray = Car.create(name: "Camray", year: 2007, color: "White", style: "sedan", company_id: toyota.id)
-avalon = Car.create(name: "Avalon", year: 2010, color: "Black", style: "sedan", company_id: toyota.id)
-altima = Car.create(name: "Altima", year: 2017, color: "Blue", style: "sedan", company_id: nissan.id)
+# camray = Car.create(name: "Camray", year: 2007, color: "White", style: "sedan", company_id: toyota.id)
+# avalon = Car.create(name: "Avalon", year: 2010, color: "Black", style: "sedan", company_id: toyota.id)
+# altima = Car.create(name: "Altima", year: 2017, color: "Blue", style: "sedan", company_id: nissan.id)
 
 Image.destroy_all
-camray_img = Image.create(url: "", car_id: camray.id)
-avalon_img = Image.create(url: "", car_id: avalon.id)
-altima_img = Image.create(url: "", car_id: altima.id)
+# camray_img = Image.create(url: "", car_id: camray.id)
+# avalon_img = Image.create(url: "", car_id: avalon.id)
+# altima_img = Image.create(url: "", car_id: altima.id)
